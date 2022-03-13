8a994694636a758dd302e43a053754f8f84a4947
haihm <37584941+haminhhai@users.noreply.github.com>
2022-03-13 17:31:41 +0700

    create dashboard slice

---
+++ src/features/student/index.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+
+export interface StudentFeatureProps {
+}
+
+export default function StudentFeature (props: StudentFeatureProps) {
+  return (
+    <div>
+      Student Feature
+    </div>
+  );
+}
