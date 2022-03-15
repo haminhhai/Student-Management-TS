f5e8e2869676cb6b07c2c3862773a174014f9eed
haihm <37584941+haminhhai@users.noreply.github.com>
2022-03-16 00:17:13 +0700

    logic saga for list student

--- src/app/rootSaga.ts
+++ src/app/rootSaga.ts
@@ -1,8 +1,9 @@
-import {all} from 'redux-saga/effects'
+import { all } from 'redux-saga/effects';
 import counterSaga from 'features/counter/counterSaga';
 import authSaga from 'features/auth/authSaga';
 import dashboardSaga from 'features/dashboard/dashboardSaga';
+import studentSaga from 'features/student/studentSaga';
 
 export default function* RootSaga() {
-  yield all([counterSaga(), authSaga(), dashboardSaga()])
+  yield all([counterSaga(), authSaga(), dashboardSaga(), studentSaga()]);
 }
---
+++ src/features/student/studentSaga.ts
@@ -0,0 +1,18 @@
+import { PayloadAction } from "@reduxjs/toolkit";
+import studentApi from "api/student";
+import { ListParams, ListResponse, Student } from "models";
+import { call, put, takeLatest } from "redux-saga/effects";
+import { studentActions } from "./studentSlice";
+
+function* fetchStudentList(action: PayloadAction<ListParams>) {
+    try {
+        const response: ListResponse<Student> = yield call(studentApi.getAll, action.payload);
+        yield put(studentActions.fetchStudentListSuccess(response));
+    } catch (error) {
+        yield put(studentActions.fetchStudentListFailed());
+    }
+}
+
+export default function* studentSaga() {
+    yield takeLatest(studentActions.fetchStudentList.type, fetchStudentList);
+}
